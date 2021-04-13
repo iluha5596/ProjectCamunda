a6e453991ebabccb02f635d9327662fda717ae65
iluha5596 <pantik96@mail.ru>
2021-04-13 18:33:58 +0300

    Sart_Camunda

--- src/main/java/camunda/Application.java
+++ src/main/java/camunda/Application.java
@@ -1,4 +1,4 @@
-package com.reunico.demo;
+package camunda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
-  public static void main(String[] args) {
-    SpringApplication.run(Application.class);
+  public static void main(String... args) {
+    SpringApplication.run(Application.class, args);
   }
 
 }
